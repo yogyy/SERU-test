cce5510f8d61d55b5087af1cb90aad84be938827
yogyy <yogimr261@gmail.com>
2024-07-04 23:34:02 +0700

    chore: update server, fr

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -10,7 +10,7 @@ export default async function buildServer() {
 
   app.register(import("@/plugins/auth"));
   app.register(import("@/routes/auth.routes"), { prefix: "/api/auth" });
-  app.register(import("@/routes/vehicle"), { prefix: "/api" });
+  app.register(import("@/routes/vehicle.route"), { prefix: "/api" });
 
   return app;
 }
