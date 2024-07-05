14680ec51d9ed7577c88b8be1e2a47ba2be125cf
yogyy <yogimr261@gmail.com>
2024-07-05 17:49:44 +0700

    chore: update migration

--- backend/src/db/migrations/001.create_db.ts
+++ backend/src/db/migrations/001.create_db.ts
@@ -4,7 +4,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("users")
     .addColumn("id", "serial", (col) => col.primaryKey())
-    .addColumn("name", "varchar(100)", (col) => col.notNull())
+    .addColumn("name", "varchar(100)", (col) => col.unique().notNull())
     .addColumn("is_admin", "boolean", (col) => col.defaultTo(false).notNull())
     .addColumn("created_at", "timestamp", (col) =>
       col.defaultTo(sql`current_timestamp`).notNull()
@@ -17,7 +17,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("vehicle_brand")
     .addColumn("id", "serial", (col) => col.primaryKey())
-    .addColumn("name", "varchar", (col) => col.notNull())
+    .addColumn("name", "varchar(100)", (col) => col.notNull())
     .addColumn("created_at", "timestamp", (col) =>
       col.defaultTo(sql`current_timestamp`).notNull()
     )
@@ -29,7 +29,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("vehicle_type")
     .addColumn("id", "serial", (col) => col.primaryKey())
-    .addColumn("name", "varchar", (col) => col.notNull())
+    .addColumn("name", "varchar(100)", (col) => col.notNull())
     .addColumn("brand_id", "integer", (col) =>
       col.references("vehicle_brand.id")
     )
@@ -44,7 +44,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("vehicle_model")
     .addColumn("id", "serial", (col) => col.primaryKey())
-    .addColumn("name", "varchar", (col) => col.notNull())
+    .addColumn("name", "varchar(100)", (col) => col.notNull())
     .addColumn("type_id", "integer", (col) =>
       col.references("vehicle_type.id").notNull()
     )
@@ -71,8 +71,8 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("pricelist")
     .addColumn("id", "serial", (col) => col.primaryKey())
-    .addColumn("code", "varchar", (col) => col.notNull())
-    .addColumn("price", "varchar", (col) => col.notNull())
+    .addColumn("code", "varchar(100)", (col) => col.notNull())
+    .addColumn("price", "varchar(100)", (col) => col.notNull())
     .addColumn("year_id", "integer", (col) =>
       col.references("vehicle_year.id").notNull()
     )
--- backend/src/db/migrations/002.make-user.name-unique.ts
+++
@@ -1,8 +0,0 @@
-import { Kysely } from "kysely";
-
-export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema
-    .alterTable("users")
-    .addUniqueConstraint("unique_user.name", ["name"])
-    .execute();
-}
